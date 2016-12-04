4bedfc17050d84eb1dfa15c90699165626e04289
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2016-12-04 12:42:32 -0200

    first commit

---
+++ src/threads/Principal.java
@@ -0,0 +1,13 @@
+package threads;
+
+public class Principal {
+	
+	public static void main(String[] args) throws Exception {
+		
+		System.out.println("THREAD MAIN");
+		
+		Thread.sleep(50000);
+		
+	}
+
+}
